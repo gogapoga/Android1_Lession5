fa781512c124dd3ce16f542fe6e96854ff61e2ea
gogapoga <gogapoga1981@yandex,ru>
2019-01-21 20:37:34 +0300

    ДЗ к 5-му уроку

---
+++ app/src/main/java/ru/gb/android1_lession4/Russian.java
@@ -0,0 +1,21 @@
+package ru.gb.android1_lession4;
+
+public class Russian extends Language {
+    public Russian() {
+        wordWeather = "Погода     ";
+        wordDate = "ru-RU";
+        wordSettings = "Настройки";
+        wordLanguage = "Язык:";
+        wordStyle = "Стиль:";
+        wordTime = "Время: ";
+        wordTemperature = "Температура: ";
+        wordWind = "Ветер";
+        wordPressure = "Давление";
+        wordHumidity = "Влажность";
+        wordMMPTST = " мм. рт. ст.";
+        wordMC = " м/с";
+        wordC = " °C";
+        wordDay = "День";
+        wordNight = "Ночь";
+    }
+}
